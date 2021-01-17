50b2c180a85aa4a721f8dc2dcf2ab58af238e6b7
Rohan Pillai <32511124+rohanpillai20@users.noreply.github.com>
2021-01-17 10:47:18 -0500

    Update ApiRequest.java

--- src/main/java/ApiRequest.java
+++ src/main/java/ApiRequest.java
@@ -1,67 +1,32 @@
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+import com.google.gson.*;
+        
+try{
+    URL obj = new URL("https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=" + topic);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    con.setRequestMethod("GET");
+    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    
+    
+    StringBuilder response = new StringBuilder();
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-
-public class ApiRequest {
-
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter topic: ");
-        String topic = in.nextLine();
-
-        System.out.println(getTopicCount(topic));
+    while ((inputLine = in.readLine()) != null) {
+        response.append(inputLine);
     }
 
-    public static int getTopicCount(String topic) {
-        int count = 0;
-        String apiUrl = "https://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&format=json&page=" + topic;
+    Gson gson = new Gson();
+    JsonObject jsonObject = gson.fromJson(response.toString(), JsonObject.class);
 
-        HttpURLConnection connection = null;
-
-        try {
-            //Create connection
-            URL url = new URL(apiUrl);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            String inputLine;
-            StringBuilder response = new StringBuilder();
-
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-
-            in.close();
-
-            Gson gson = new Gson();
-            JsonObject jsonObject = gson.fromJson(response.toString(), JsonObject.class);
-
-            String[] textArray =
-                    jsonObject.getAsJsonObject("parse").get("text").toString().replaceAll("[^a-zA-Z0-9]+", " ")
-                              .split(" ");
-
-            for (String s : textArray) {
-                if (topic.equals(s)) {
-                    count++;
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
-        }
-
-        return count;
+    String text = jsonObject.getAsJsonObject("parse").get("text").toString();
+            
+    int index = text .indexOf(topic);
+    while (index >= 0) {
+        count++;
+        index = text.indexOf(topic, index + 1);
     }
+
+}catch(Exception e){
+    e.printStackTrace();
 }
-
